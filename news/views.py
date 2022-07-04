08365efc1f9714adac3ade622330e223d79ed372
sshmind <sashadevlnx@gmail.com>
2022-07-04 08:46:50 +0430

    change add_data function parameter's name some_dict -> article

--- news/views.py
+++ news/views.py
@@ -7,21 +7,21 @@ from .models import Author, Article, Tag
 
 
 
-def add_data(some_dict):
+def add_data(article):
     """serializer operations"""
-    author_serializer = AuthorSerializer(data=some_dict['author'])
+    author_serializer = AuthorSerializer(data=article['author'])
     if author_serializer.is_valid():
         author_serializer.save()
     
-    if 'tags' in some_dict.keys():
-        for tag in some_dict['tags']:
+    if 'tags' in article.keys():
+        for tag in article['tags']:
             tag_serializer = TagSerializer(data={'name': tag})
             if tag_serializer.is_valid():
                 tag_serializer.save()
     
 
 
-    article_serializer = ArticleSerializer(data=some_dict)
+    article_serializer = ArticleSerializer(data=article)
     if article_serializer.is_valid():
         article_serializer.save()
 

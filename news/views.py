9217910c52c6a9e3b7d7f8f36a35af6f74abd99c
sshmind <sashadevlnx@gmail.com>
2022-07-04 08:44:41 +0430

    add add_data function for serializer operations

--- news/views.py
+++ news/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
 
-# Create your views here.
+from .serializers import AuthorSerializer, TagSerializer, ArticleSerializer
+from .models import Author, Article, Tag
+
+
+
+def add_data(some_dict):
+    """serializer operations"""
+    author_serializer = AuthorSerializer(data=some_dict['author'])
+    if author_serializer.is_valid():
+        author_serializer.save()
+    
+    if 'tags' in some_dict.keys():
+        for tag in some_dict['tags']:
+            tag_serializer = TagSerializer(data={'name': tag})
+            if tag_serializer.is_valid():
+                tag_serializer.save()
+    
+
+
+    article_serializer = ArticleSerializer(data=some_dict)
+    if article_serializer.is_valid():
+        article_serializer.save()
+
+

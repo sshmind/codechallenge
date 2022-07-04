b7307252c54c109b940caf1dbe840b666813f807
sshmind <sashadevlnx@gmail.com>
2022-07-04 08:27:08 +0430

    config django admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Tag, Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    
+    list_display = ['title', 'author', 'date_published']
+    list_filter = ('author', 'date_published', 'date_modified')
+    search_fields = ('title', 'author__name')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'url')
+    search_fields = ('name',)
